09f6711be6d90f540ba1cf2dd00ed8beee5cd60a
LinkAnJarad <79294502+LinkAnJarad@users.noreply.github.com>
2022-09-30 09:04:28 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     version=VERSION,
     author="Link An Jarad",
     description=DESCRIPTION,
+    url="https://github.com/LinkAnJarad/global_local_attention_module_pytorch",
     packages=find_packages(),
     install_requires=['torch']
 )
