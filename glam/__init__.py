a385dcc6559e492e3dfb9a9f5cbd81624b3cd703
LinkAnJarad <79294502+LinkAnJarad@users.noreply.github.com>
2022-09-30 08:57:51 +0800

    Delete glam directory

--- glam/__init__.py
+++
@@ -1,5 +0,0 @@
-from .glam import GLAM
-from .local_channel_attention import LocalChannelAttention
-from .global_channel_attention import GlobalChannelAttention
-from .local_spatial_attention import LocalSpatialAttention
-from .global_spatial_attention import GlobalSpatialAttention
